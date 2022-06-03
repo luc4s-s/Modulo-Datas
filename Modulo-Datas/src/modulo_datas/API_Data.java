cc8508f4343f8177bc3dd796e76db1d7f6346988
lucas gomes silava <lucasgomessilva713@gmail.com>
2022-06-03 13:27:06 -0300

    Formatando data e hora com nova API

--- Modulo-Datas/src/modulo_datas/API_Data.java
+++ Modulo-Datas/src/modulo_datas/API_Data.java
@@ -3,6 +3,7 @@ package modulo_datas;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class API_Data {
 
@@ -10,18 +11,37 @@ public class API_Data {
 		// nova API de data 
 		
 		/** gerando a data atual **/
+		System.out.println("---FORMATADA---");
 		LocalDate dataAtual = LocalDate.now();
-		System.out.println("data atual: " + dataAtual);
+		System.out.println("data atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		
 		
 		/** gerando a hora atual **/
 		LocalTime horaAtual = LocalTime.now();
-		System.out.println("Hora Atual: " + horaAtual);
+		System.out.println("Hora Atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm")));
 		
 		
 		/** data e hora juntos **/
 		LocalDateTime dataAtualhoraAtual = LocalDateTime.now();
-		System.out.println("Data e Hora Atua : " + dataAtualhoraAtual);
+		System.out.println("Data e Hora Atua : " + dataAtualhoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) + "\n");
+	
+	
+		// Data e Hora nao formatada 
+		
+		/** gerando a data atual **/
+		System.out.println("---NÃO FORMATADA---");
+		LocalDate dataAtual0 = LocalDate.now();
+		System.out.println("data atual: " + dataAtual0);
+		
+		
+		/** gerando a hora atual **/
+		LocalTime horaAtual0 = LocalTime.now();
+		System.out.println("Hora Atual: " + horaAtual0);
+		
+		
+		/** data e hora juntos **/
+		LocalDateTime dataAtualhoraAtual0 = LocalDateTime.now();
+		System.out.println("Data e Hora Atua : " + dataAtualhoraAtual0);
 	}
 
 }
